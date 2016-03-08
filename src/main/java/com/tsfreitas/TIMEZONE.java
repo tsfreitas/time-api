d2d3c0a9bdfc3a47eede8b5c64c58a27769821e7
THIAGO SANTOS DE FREITAS <tsfreitas@gmail.com>
2016-03-08 08:30:10 -0300

    first commit

---
+++ src/main/java/com/tsfreitas/TIMEZONE.java
@@ -0,0 +1,17 @@
+package com.tsfreitas;
+
+public enum TIMEZONE {
+
+	SAO_PAULO("America/Sao_Paulo"), AUCKLAND("Pacific/Auckland"), NEW_YORK("America/New_York");
+
+	private String zone;
+
+	private TIMEZONE(String zone) {
+		this.zone = zone;
+	}
+	
+	public String getZone() {
+		return zone;
+	}
+
+}
---
+++ src/main/java/com/tsfreitas/TimeApiApplication.java
@@ -0,0 +1,15 @@
+package com.tsfreitas;
+
+import java.io.IOException;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeApiApplication {
+
+	public static void main(String[] args) throws IOException {
+//		http://api.timezonedb.com/?zone=Australia/Melbourne&key=NELQKJ8YF4YG&format=json
+		
+//		SpringApplication.run(TimeApiApplication.class, args);
+	}
+}
