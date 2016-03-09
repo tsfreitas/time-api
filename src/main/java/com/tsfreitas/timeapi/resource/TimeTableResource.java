0989396f27b26cd9ad142fd7fd4634a634e38b87
THIAGO SANTOS DE FREITAS <thiago.freitas@buscapecompany.com>
2016-03-09 19:17:43 -0300

    Correcao de chamada Retrofit

--- src/main/java/com/tsfreitas/timeapi/resource/TimeTableResource.java
+++ src/main/java/com/tsfreitas/timeapi/resource/TimeTableResource.java
@@ -6,8 +6,6 @@ import retrofit2.http.Query;
 
 public interface TimeTableResource {
 
-	// http://api.timezonedb.com/?zone=Australia/Melbourne&key=NELQKJ8YF4YG&format=json
-
 	@GET("/?format=json")
 	public Call<Object> getTimeZone(@Query("zone") String zone);
 
